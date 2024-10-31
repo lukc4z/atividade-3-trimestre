35dc61da99cef173273dd90442c27d4ff50fd69d
lukc4z <151041424+lukc4z@users.noreply.github.com>
2024-10-30 21:08:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Matemática',
+    'quantos segundos se tem em 1 ano?',
+    'Um ano tem 31.536.000 segundos'
+)
+
+criaCartao(
+    'Geografia',
+    'quantos continentes existem no mundo?',
+    'seis'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual o melhor livro que você já leu?',
+    '"As cronicas de Narnia" C.S. Lewis'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que vem após a morte?',
+    'O que vem após a morte é um mistério. Diferentes culturas e religiões oferecem explicações variadas, como a ideia de vida após a morte, reencarnação ou um estado de paz. A ciência, por sua vez, foca na cessação das funções biológicas. No fim, a resposta depende das crenças pessoais e filosóficas de cada um.'
+)
+criaCartao(
+    'quimica',
+    'qual a formula da água?',
+    'dois atomos de hidrogênio e um de oxigênio'
+)
+criaCartao(
+     'biologia',
+     'quantos cromossomos temos?',
+     '23 pares ou 46 '
+)
